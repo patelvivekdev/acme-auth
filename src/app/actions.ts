7066e55f38412a60951cc198c7e5568d021fe3da
patelvk2000 <patelvk2000@gmail.com>
2024-03-26 01:53:38 +0000

    added change password

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,273 +1,304 @@
-'use server'
+"use server";
 
-import { redirect } from 'next/navigation'
-import { cookies } from 'next/headers'
-import { z } from 'zod';
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { z } from "zod";
+
+const cookieStore = cookies();
+const accessToken = cookieStore.get("accessToken");
+const refreshToken = cookieStore.get("refreshToken");
+
+// define responce type
+type Response = {
+  ststusCdoe: number;
+  data: any;
+  message: string;
+  success: boolean;
+};
 
 const registerSchema = z.object({
-	username: z.string().min(3, { message: "Must be 3 or more characters long" }),
-	email: z.string().email("Please enter valid message").min(5),
-	password: z.string().min(8, { message: "Must be 8 or more characters long" }),
-	role: z.enum(['ADMIN', 'USER']),
+  username: z.string().min(3, { message: "Must be 3 or more characters long" }),
+  email: z.string().email("Please enter valid message").min(5),
+  password: z.string().min(8, { message: "Must be 8 or more characters long" }),
+  role: z.enum(["ADMIN", "USER"]),
 });
 
-const loginSchema = z.object({
-	email: z.string().email("Please enter valid email"),
-	password: z.string()
-})
-
-
 export async function register(prevState: any, formData: FormData) {
+  const validatedFields = registerSchema.safeParse({
+    email: formData.get("email"),
+    username: formData.get("username"),
+    password: formData.get("password"),
+    role: formData.get("role"),
+  });
 
-	const validatedFields = registerSchema.safeParse({
-		email: formData.get('email'),
-		username: formData.get('username'),
-		password: formData.get('password'),
-		role: formData.get('role'),
-	})
+  // Return early if the form data is invalid
+  if (!validatedFields.success) {
+    return {
+      type: "error",
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to register.",
+    };
+  }
 
-	// Return early if the form data is invalid
-	if (!validatedFields.success) {
-		return {
-			type: 'error',
-			errors: validatedFields.error.flatten().fieldErrors,
-			message: 'Missing Fields. Failed to register.',
-		};
-	}
+  try {
+    const response = await fetch(
+      "https://api.freeapi.app/api/v1/users/register",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(validatedFields.data),
+      }
+    );
 
-	try {
-		const response = await fetch(
-			'https://api.freeapi.app/api/v1/users/register',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify(validatedFields.data),
-			}
-		)
+    const result: Response = await response.json();
 
-		const result = await response.json()
-
-		if (result.success === false) {
-			return {
-				type: 'error',
-				message: result.message
-			}
-		}
-	} catch (error: any) {
-		console.log('error', error.message)
-		return {
-			type: 'error',
-			message: 'Database Error: Failed to register.',
-		};
-	}
-	redirect('/login')
+    if (result.success === false) {
+      return {
+        type: "error",
+        message: result.message,
+      };
+    }
+  } catch (error: any) {
+    console.log("Error", error.message);
+    return {
+      type: "error",
+      message: "Database Error: Failed to register.",
+    };
+  }
+  redirect("/login");
 }
 
+const loginSchema = z.object({
+  email: z.string().email("Please enter valid email"),
+  password: z.string(),
+});
+
 export async function login(prevState: any, formData: FormData) {
+  const validatedFields = loginSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
 
-	const validatedFields = loginSchema.safeParse({
-		email: formData.get('email'),
-		password: formData.get('password'),
-	})
+  // Return early if the form data is invalid
+  if (!validatedFields.success) {
+    return {
+      type: "error",
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to login.",
+    };
+  }
 
-	// Return early if the form data is invalid
-	if (!validatedFields.success) {
-		return {
-			type: 'error',
-			errors: validatedFields.error.flatten().fieldErrors,
-			message: 'Missing Fields. Failed to login.',
-		};
-	}
+  try {
+    const response = await fetch("https://api.freeapi.app/api/v1/users/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(validatedFields.data),
+    });
 
-	try {
-		const email = formData.get('email')
-		const password = formData.get('password')
+    const result: Response = await response.json();
 
-		const response = await fetch(
-			'https://api.freeapi.app/api/v1/users/login',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					email,
-					password,
-				}),
-			}
-		)
-
-		const result = await response.json()
-
-		// // Check if success is true, if true set cookie and redirect to /
-		if (result.success) {
-			// Set cookie
-			const oneDay = 24 * 60 * 60 * 1000
-			cookies().set({
-				name: 'accessToken',
-				value: result.data.accessToken,
-				httpOnly: true,
-				secure: true,
-				expires: new Date(Date.now() + oneDay),
-			})
-			cookies().set({
-				name: 'refreshToken',
-				value: result.data.refreshToken,
-				httpOnly: true,
-				secure: true,
-				expires: new Date(Date.now() + oneDay),
-			})
-		} else {
-			return {
-				type: 'error',
-				message: result.message
-			}
-		}
-	} catch (error: any) {
-		console.log('error', error.message)
-		return {
-			type: 'error',
-			message: 'Database Error: Failed to login.',
-		};
-	}
-	redirect('/profile')
+    // // Check if success is true, if true set cookie and redirect to /
+    if (result.success) {
+      // Set cookie
+      const oneDay = 24 * 60 * 60 * 1000;
+      cookies().set({
+        name: "accessToken",
+        value: result.data.accessToken,
+        httpOnly: true,
+        secure: true,
+        expires: new Date(Date.now() + oneDay),
+      });
+      cookies().set({
+        name: "refreshToken",
+        value: result.data.refreshToken,
+        httpOnly: true,
+        secure: true,
+        expires: new Date(Date.now() + oneDay),
+      });
+    } else {
+      return {
+        type: "error",
+        message: result.message,
+      };
+    }
+  } catch (error: any) {
+    console.log("Error", error.message);
+    return {
+      type: "error",
+      message: "Database Error: Failed to login.",
+    };
+  }
+  redirect("/profile");
 }
 
 export async function logout() {
-	try {
-		cookies().delete('accessToken')
-		cookies().delete('refreshToken')
-	} catch (error) {
-		console.log("Error", error)
-	}
+  try {
+    cookies().delete("accessToken");
+    cookies().delete("refreshToken");
+  } catch (error: any) {
+    console.log("Error", error.message);
+  }
 }
 
 // Fetch data from API
 export async function getCurrentUser() {
-	const cookieStore = cookies()
-	const accessToken = cookieStore.get('accessToken')
+  try {
+    const response = await fetch(
+      "https://api.freeapi.app/api/v1/users/current-user",
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken?.value}`,
+        },
+      }
+    );
 
-	try {
-		const res = await fetch('https://api.freeapi.app/api/v1/users/current-user',
-			{
-				headers: {
-					"Content-Type": "application/json",
-					"Authorization": `Bearer ${accessToken?.value}`
-				},
-			}
-		)
+    const result: Response = await response.json();
 
-		const data: {
-			ststusCdoe: number,
-			data: any,
-			message: string,
-			success: boolean
-		} = await res.json()
-
-		return data
-
-	} catch (error) {
-		console.log("error", error)
-	}
+    return result;
+  } catch (error) {
+    console.log("error", error);
+  }
 }
 
-
 export async function forgetPassword(formData: FormData) {
-	try {
-		const email = formData.get('email')
+  try {
+    const email = formData.get("email");
 
-		const response = await fetch(
-			'https://api.freeapi.app/api/v1/users/forgot-password',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					email,
-				}),
-			}
-		)
+    const response = await fetch(
+      "https://api.freeapi.app/api/v1/users/forgot-password",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      }
+    );
 
-		const result = await response.json()
-	} catch (error) {
-		console.log('error', error)
-	}
+    const result: Response = await response.json();
+
+    return result;
+  } catch (error) {
+    console.log("error", error);
+  }
 }
 
 export async function resendEmailVerification() {
-	const cookieStore = cookies()
-	const accessToken = cookieStore.get('accessToken')?.value
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
 
-	try {
-		if (accessToken) {
-			const response = await fetch(
-				'https://api.freeapi.app/api/v1/users/resend-email-verification',
-				{
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json',
-					},
-				}
-			)
+  try {
+    if (accessToken) {
+      const response = await fetch(
+        "https://api.freeapi.app/api/v1/users/resend-email-verification",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
-			const result = await response.json()
-		} else {
-			throw new Error('Invalid token! Plasea login again')
-		}
-	} catch (error) {
-		console.log('Error', error)
-	}
+      const result: Response = await response.json();
+
+      return result;
+    } else {
+      throw new Error("Invalid token! Plasea login again");
+    }
+  } catch (error) {
+    console.log("Error", error);
+  }
 }
 
-export async function changePassword(formData: FormData) {
-	try {
-		const oldPassword = formData.get('oldPassword')
-		const newPassword = formData.get('newPassword')
+const changePasswordSchema = z.object({
+  oldPassword: z.string(),
+  newPassword: z
+    .string()
+    .min(8, { message: "Must be 8 or more characters long" }),
+  newPassword2: z
+    .string()
+    .min(8, { message: "Must be 8 or more characters long" }),
+});
 
-		const response = await fetch(
-			'https://api.freeapi.app/api/v1/users/change-password',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					oldPassword,
-					newPassword,
-				}),
-			}
-		)
+export async function changePassword(prevState: any, formData: FormData) {
+  const validatedFields = changePasswordSchema.safeParse({
+    oldPassword: formData.get("oldPassword"),
+    newPassword: formData.get("newPassword"),
+    newPassword2: formData.get("newPassword2"),
+  });
 
-		const result = await response.json()
-	} catch (error) {
-		console.log('Error', error)
+  // Return early if the form data is invalid
+  if (!validatedFields.success) {
+    return {
+      type: "error",
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Change the password.",
+    };
+  }
+
+  if (formData.get("newPassword") !== formData.get("newPassword2")) {
+    return {
+      type: "error",
+      message: "Password does not match",
+    };
+  }
+
+  try {
+    const response = await fetch(
+      "https://api.freeapi.app/api/v1/users/change-password",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken?.value}`,
+        },
+        body: JSON.stringify(validatedFields.data),
+      }
+    );
+
+    const result: Response = await response.json();
+    console.log("result", result);
+	if(result.success===false){
+		return {
+			type:"error",
+			message:result.message
+		}
 	}
+
+    return result;
+  } catch (error) {
+    console.log("Error", error);
+  }
 }
 
 export async function changeAvatar(formData: FormData) {
-	try {
-		const avatar = formData.get('file')
+  try {
+    const avatar = formData.get("file");
 
-		const response = await fetch(
-			'https://api.freeapi.app/api/v1/users/avatar',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'multipart/form-data',
-				},
-				body: avatar,
-			}
-		)
+    const response = await fetch(
+      "https://api.freeapi.app/api/v1/users/avatar",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+        body: avatar,
+      }
+    );
 
-		const result = await response.json()
-	} catch (error) {
-		console.log('Error', error)
-	}
+    const result: Response = await response.json();
+
+    return result;
+  } catch (error) {
+    console.log("Error", error);
+  }
 }
 
-export async function verifyEmail(formData: FormData) {
-
-}
+export async function verifyEmail(formData: FormData) {}
--- src/app/profile/change-password/page.tsx
+++ src/app/profile/change-password/page.tsx
@@ -1,13 +1,90 @@
+"use client";
+import Link from "next/link";
+import { useFormState } from "react-dom";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { SubmitButton } from "@/components/submit-button";
+import { changePassword } from "@/app/actions";
+
+const initialState = {
+  message: "",
+  errors: null,
+};
+
 const ChnagePasswordPage = () => {
+  const [state, formAction] = useFormState<any>(
+    changePassword as any,
+    initialState
+  );
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="mx-auto grid w-[350px] gap-6">
         <div className="grid gap-2 text-center">
-          <h1 className="text-3xl font-bold">Login</h1>
+          <h1 className="text-3xl font-bold">Change Password</h1>
           <p className="text-balance text-muted-foreground">
-            Enter your email below to login to your account
+            Change your password!
           </p>
         </div>
+        <form action={formAction}>
+          <div className="grid gap-4">
+            {state?.type === "error" && (
+              <p className="text-lg mb-2 bg-green-951 text-red-600 border-2 border-gray-300 rounded-md p-2 my-4">
+                {state.message}
+              </p>
+            )}
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="oldPassword">Password</Label>
+              </div>
+              <Input
+                id="oldPassword"
+                type="password"
+                required
+                name="oldPassword"
+              />
+              {state?.errors?.oldPassword && (
+                <span id="oldPassword-error" className="text-red-600 text-sm">
+                  {state.errors.oldPassword.join(",")}
+                </span>
+              )}
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="newPassword">newPassword</Label>
+              </div>
+              <Input
+                id="newPassword"
+                type="password"
+                required
+                name="newPassword"
+              />
+              {state?.errors?.newPassword && (
+                <span id="newPassword-error" className="text-red-600 text-sm">
+                  {state.errors.newPassword.join(",")}
+                </span>
+              )}
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="newPassword2">Repet your password</Label>
+              </div>
+              <Input
+                id="newPassword2"
+                type="password"
+                required
+                name="newPassword2"
+              />
+              {state?.errors?.newPassword2 && (
+                <span id="newPassword2-error" className="text-red-600 text-sm">
+                  {state.errors.newPassword2.join(",")}
+                </span>
+              )}
+            </div>
+            <SubmitButton name="Change password" />
+          </div>
+        </form>
       </div>
     </div>
   );
