ebf916a42e4ac19a0bbc085571a3478417c56b21
patelvk2000 <patelvk2000@gmail.com>
2024-03-26 06:38:35 +0000

    added  Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from "@vercel/analytics/react";
 
 import { UserProvider } from "@/components/UserContext";
 
@@ -54,6 +55,7 @@ export default function RootLayout({
                 <div className="h-full w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center lg:grid lg:grid-cols-1">
                   <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_70%,black)] sm:[mask-image:radial-gradient(ellipse_at_center,transparent_30%,black)]"></div>
                   {children}
+                  <Analytics />
                 </div>
               </main>
             </div>
